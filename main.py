8b31b2a8274a25bc1bc16dd57f98a2283f36aa67
Evan Lin <evan.if.lin@linecorp.com>
2024-08-28 16:11:48 +0800

    Refactor handle_callback function in main.py to include logging user ID and URL detection

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ async def handle_callback(request: Request):
 
 async def handle_message_event(event: MessageEvent):
     user_id = event.source.user_id
-
+    logger.info(f"UID: {user_id}")
     url = find_url(event.message.text)
     logger.info(f"URL: >{url}<")
     if find_url(event.message.text) != '':
