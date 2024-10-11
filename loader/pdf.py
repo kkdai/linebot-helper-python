c6b5c962e0b0f269c8fa2472c6c7f138886b4d43
Evan Lin <evan.if.lin@linecorp.com>
2024-10-11 17:05:59 +0800

    Add PDF loader and utility function
    This commit adds a new PDF loader module and a utility function for converting a list of documents to a string. The PDF loader module allows loading PDF files from a given URL and converting them to text using the PyPDFLoader library. The utility function takes a list of Document objects and returns a string representation of their page content.
    
    Co-authored-by: Narumi

---
+++ loader/pdf.py
@@ -0,0 +1,32 @@
+
+import tempfile
+
+import httpx
+from langchain_community.document_loaders.pdf import PyPDFLoader
+from loguru import logger
+
+from utils import docs_to_str
+
+
+def load_pdf(url: str) -> str:
+    logger.info("Loading PDF: {}", url)
+
+    headers = {
+        "Accept-Language": "zh-TW,zh;q=0.9,ja;q=0.8,en-US;q=0.7,en;q=0.6",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",  # noqa
+        "Cookie": "over18=1",  # ptt
+    }
+
+    resp = httpx.get(url=url, headers=headers, follow_redirects=True)
+    resp.raise_for_status()
+
+    suffix = ".pdf" if resp.headers.get(
+        "content-type") == "application/pdf" else None
+    with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as fp:
+        fp.write(resp.content)
+
+    return docs_to_str(PyPDFLoader(fp.name).load())
+
+
+def load_pdf_file(f: str) -> str:
+    return docs_to_str(PyPDFLoader(f).load())
