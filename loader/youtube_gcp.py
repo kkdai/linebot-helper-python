cb87e5c2aaea82677481a79910b097be047e3d81
Evan Lin <evan.if.lin@linecorp.com>
2024-10-13 21:02:15 +0800

    Refactor langtools.py and loader/youtube_gcp.py
    Remove unused summarize_url_with_sherpa function from langtools.py and move fetch_youtube_data_from_gcp function to loader/youtube_gcp.py. Also remove unnecessary imports and improve code readability.

--- loader/youtube_gcp.py
+++ loader/youtube_gcp.py
@@ -1,3 +1,7 @@
+import re
+import os
+import logging
+import requests
 
 
 def load_from_youtube(youtube_url: str) -> str:
@@ -18,7 +22,6 @@ def load_from_youtube(youtube_url: str) -> str:
             ids_data = result['ids_data']
             logging.debug(
                 f"ids_data data: {ids_data[:50]}")
-            summary = summarize_text(ids_data)
         else:
             logging.error("ids_data not found in result:", result)
             summary = "Error or ids_data not found..."
@@ -26,3 +29,28 @@ def load_from_youtube(youtube_url: str) -> str:
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}", exc_info=True)
         return "error:"+str(e)
+
+
+def fetch_youtube_data_from_gcp(video_id):
+    try:
+        # Read the URL from the environment variable
+        url = os.environ.get('GCP_LOADER_URL')
+        if not url:
+            return {"error": "Environment variable 'GCP_LOADER_URL' is not set"}
+
+        # Define the parameters
+        params = {'v_id': video_id}
+
+        # Make the GET request
+        response = requests.get(url, params=params)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            return data
+        else:
+            # Handle errors
+            return {"error": f"Request failed with status code {response.status_code}"}
+    except Exception as e:
+        return {"error": str(e)}
